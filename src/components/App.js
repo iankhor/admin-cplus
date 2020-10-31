4fab34abea617fe13db52e0c371b505c472ab232
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-11-01 10:08:33 +1100

    First cut to display summarised financials

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
-import React, { Component, useState } from 'react'
-import { Container, ListGroup } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, ListGroup, Button, Table } from 'react-bootstrap'
 import practitioners from './../data/practitioners.json'
+import appointments from './../data/appointments.json'
 import { formatISO } from 'date-fns'
+import { summariseFinancials, findPracitioner } from './../lib'
 
 import DayPickerInput from 'react-day-picker/DayPickerInput'
 import 'react-day-picker/lib/style.css'
@@ -10,6 +12,24 @@ export default function App() {
   const [selected, setSelected] = useState([])
   const [startDate, setStartDate] = useState(null)
   const [endDate, setEndDate] = useState(null)
+  const [financials, setFinancials] = useState([])
+
+  function generateFinancials() {
+    const financials = selected.map((practitionerId) => {
+      const summary = summariseFinancials(appointments, practitionerId, {
+        startDate: formatISO(startDate, { representation: 'date' }),
+        endDate: formatISO(endDate, { representation: 'date' }),
+      })
+
+      return {
+        practitionerId,
+        practitionerName: findPracitioner(practitionerId, practitioners).name,
+        ...summary,
+      }
+    })
+
+    setFinancials(financials)
+  }
 
   return (
     <Container>
@@ -27,9 +47,37 @@ export default function App() {
       </ListGroup>
 
       <p>Start date</p>
-      <DayPickerInput onDayChange={(day) => setStartDate(formatISO(day, { representation: 'date' }))} />
+      <DayPickerInput onDayChange={(day) => setStartDate(day)} />
       <p>End date</p>
-      <DayPickerInput onDayChange={(day) => setEndDate(formatISO(day, { representation: 'date' }))} />
+      <DayPickerInput onDayChange={(day) => setEndDate(day)} />
+
+      <Button variant="primary" onClick={generateFinancials}>
+        Generate Report
+      </Button>
+
+      <hr />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Practitioner Name</th>
+            <th>Revenue</th>
+            <th>Cost</th>
+            <th>Gross Profit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {financials.map((f) => {
+            return (
+              <tr>
+                <td>{f.practitionerName}</td>
+                <td>{f.revenue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+                <td>{f.cost.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+                <td>{(f.revenue - f.cost).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
     </Container>
   )
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,28 +1,37 @@
 import { parse } from 'date-fns'
 import Fuse from 'fuse.js'
 
+export function findPracitioner(practitionerId, practitioners) {
+  return practitioners.find((p) => p.id === practitionerId)
+}
+
 export function findPracitionerAppointments(practitionerId, appointments) {
-  return appointments.filter(appt => appt.practitioner_id === practitionerId)
-} 
+  return appointments.filter((appt) => appt.practitioner_id === practitionerId)
+}
 
 export function findAppointmentsBetween(start, end, appointments) {
   const startDate = parse(start, 'yyyy-mm-dd', new Date())
   const endDate = parse(end, 'yyyy-mm-dd', new Date())
-  
-  return appointments.filter(appt => {
+
+  return appointments.filter((appt) => {
     const currDate = parse(appt.date, 'mm/dd/yyyy', new Date())
 
     return startDate <= currDate && currDate <= endDate
   })
-} 
+}
 
 export function summarise(appointments) {
+  const init = {
+    cost: 0,
+    revenue: 0,
+  }
+
   return appointments.reduce((acc, curr) => {
     return {
-      cost:  acc.cost + curr.cost,
-      revenue: acc.revenue + curr.revenue
+      cost: acc.cost + curr.cost,
+      revenue: acc.revenue + curr.revenue,
     }
-  }) 
+  }, init)
 }
 
 export function summariseFinancials(appointments, practitionerId, options) {
@@ -34,6 +43,6 @@ export function summariseFinancials(appointments, practitionerId, options) {
 
 export function searchPractitioner(practitioners, searchString) {
   const fuse = new Fuse(practitioners, { keys: ['name'] })
-  
-  return fuse.search(searchString).map(p => p.item)
+
+  return fuse.search(searchString).map((p) => p.item)
 }
